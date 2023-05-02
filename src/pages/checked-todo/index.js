a7bd4baa499ef3ce257515134e09efc69c095da0
esther <yinghuimeng67@gmail.com>
2023-05-02 22:19:14 +0800

    --fix: change client fetch to ssr

--- src/pages/checked-todo/index.js
+++ src/pages/checked-todo/index.js
@@ -1,40 +1,27 @@
-import { useState, useEffect } from "react";
 import TodoItem from "@/components/todoItem";
 import styles from "../todo-list/style.module.scss";
 import { getAllTodos } from "@/utils/fetchApi/todo";
-import LoadingBox from "@/components/loadingBox";
-function CheckedTodo() {
-  const [todos, setTodos] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const data = await getAllTodos();
-      const todoList = [];
-      for (let key in data) {
-        if (data[key].done === true) {
-          todoList.push({ id: key, ...data[key] });
-        }
-      }
-      setTodos(todoList);
-      setIsLoading(false);
-    };
-    getData();
-  }, []);
 
+export default function CheckedTodo({ todoList }) {
   return (
     <div className={styles.container}>
-      <h1> Checked Todos</h1>
-      {isLoading ? (
-        <LoadingBox />
-      ) : (
-        <div className={styles.cardContainer}>
-          {todos.map((todo) => {
-            return <TodoItem key={todo.id} todo={todo} />;
-          })}
-        </div>
-      )}
+      <h1> Already Done!</h1>
+      <div className={styles.cardContainer}>
+        {todoList.map((todo) => {
+          return <TodoItem key={todo.id} todo={todo} />;
+        })}
+      </div>
     </div>
   );
 }
-export default CheckedTodo;
+
+export async function getServerSideProps() {
+  const res = await getAllTodos();
+  const todoList = [];
+  for (let key in res) {
+    if (res[key].done === true) {
+      todoList.push({ id: key, ...res[key] });
+    }
+  }
+  return { props: { todoList } };
+}
--- src/pages/todo-list/index.js
+++ src/pages/todo-list/index.js
@@ -1,40 +1,30 @@
 import TodoItem from "@/components/todoItem";
 import styles from "./style.module.scss";
 import { getAllTodos } from "@/utils/fetchApi/todo";
-import { useEffect, useState } from "react";
-import LoadingBox from "@/components/loadingBox";
 
-function TodoList() {
-  const [todos, setTodos] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const data = await getAllTodos();
-      const todoList = [];
-      for (let key in data) {
-        todoList.push({ id: key, ...data[key] });
-      }
-      setTodos(todoList);
-      setIsLoading(false);
-    };
-    getData();
-  }, []);
-
+export default function TodoList({ todoList }) {
+  // Render data...
   return (
     <div className={styles.container}>
       <h1> ALL My todos</h1>
-      {isLoading ? (
-        <LoadingBox />
-      ) : (
-        <div className={styles.cardContainer}>
-          {todos.map((todo) => {
-            return <TodoItem key={todo.id} todo={todo} />;
-          })}
-        </div>
-      )}
+      <div className={styles.cardContainer}>
+        {todoList.map((todo) => {
+          return <TodoItem key={todo.id} todo={todo} />;
+        })}
+      </div>
     </div>
   );
 }
-export default TodoList;
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+
+  const data = await getAllTodos();
+  const todoList = [];
+  for (let key in data) {
+    todoList.push({ id: key, ...data[key] });
+  }
+  // Pass data to the page via props
+  return { props: { todoList } };
+}
